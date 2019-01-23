6a759defa1959dbba75a7e746f968ff08fd5ce76
Jordan Smith <jordan.m.smith721@gmail.com>
2019-01-23 13:33:27 -0500

    added circumference calculation

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $f3->route('GET /', function($f3)
     $f3->set('password', sha1('Password01'));
     $f3->set('title', 'Working with templates');
     $f3->set('temp', 67);
+    $f3->set('radius', 10);
 
     //load template
     //$template = new Template();
